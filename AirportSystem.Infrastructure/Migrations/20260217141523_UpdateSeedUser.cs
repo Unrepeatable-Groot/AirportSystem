b3d1cece2251723a60b2751388cb537c9e1cd1ef
Unrepeatable-Groot <gaikarashviligiorgi5@gmail.com>
2026-02-17 18:16:27 +0400

    New Owner

---
+++ AirportSystem.Infrastructure/Migrations/20260217141523_UpdateSeedUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AirportSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSeedUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Age", "Email", "FirstName", "Gender", "LastName", "Password", "PersonalId", "PhoneNumber", "Role" },
+                values: new object[] { 1, 30, "georgedoe123@gmail.com", "George", null, "Doe", "AQAAAAIAAYagAAAAEGKvSzHABUcWL8CKhu0GFT9oI5rRqDMJmwqJkmss59gNYgnYUx1ryqtI01ahh7gUyg==", "555555555", "00000000000", 3 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+        }
+    }
+}
