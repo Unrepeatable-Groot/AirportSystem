0368fad5280fabff728005750ae8cee13acc794b
Unrepeatable-Groot <gaikarashviligiorgi5@gmail.com>
2026-02-18 01:17:35 +0400

    OwnerAcc

---
+++ AirportSystem.Infrastructure/Migrations/20260217211623_OwnerAcc.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AirportSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class OwnerAcc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Gender", "Password" },
+                values: new object[] { "male", "AQAAAAIAAYagAAAAEJd8EThBFpYcpDslkKwyv9iK3EIn0BNhO1n9phzZmmQc8Xl99GfgCchGv0F96t71qA==" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Gender", "Password" },
+                values: new object[] { null, "AQAAAAIAAYagAAAAEFp0gfLQctnR9/1SVhMuxI8Btu4PThY7LG84lksAJIrFumBc2Lfe240KfQMpp8mPJg==" });
+        }
+    }
+}
